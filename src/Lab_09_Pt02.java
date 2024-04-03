55d0376ba80584e95c093754ee64e1e04e478b80
bsauce <bsauce11@calstatela.edu>
2024-04-03 16:41:07 -0700

    lab 09 complete, updated description

--- src/Lab_09_Pt02.java
+++ src/Lab_09_Pt02.java
@@ -1,15 +1,16 @@
 public class Lab_09_Pt02 {
     /** Heap sort method */
     public static <E extends Comparable<E>> void heapSort(E[] list) {
-// Create a Heap of integers
+        // Create a Heap of Object
         Heap<E> heap = new Heap<>();
-// Add elements to the heap
+        // Add elements to the heap
         for (int i = 0; i < list.length; i++)
             heap.add(list[i]);
-// Remove elements from the heap
+        // Remove elements from the heap
         for (int i = list.length - 1; i >= 0; i--)
             list[i] = heap.remove();
     }
+
     /** A test method */
     public static void main(String[] args) {
         Integer[] list = {-44, -5, -3, 3, 3, 1, -4, 0, 1, 2, 4, 5, 53};
